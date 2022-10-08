02c826b46a23b2a154c983fc7e2848c475e4fc02
Владимир Капитаненко <vladimir_90@bk.ru>
2022-10-08 15:11:36 +0700

    new

---
+++ Les_1_Exemple_2/Program.cs
@@ -0,0 +1,16 @@
+﻿    Console.WriteLine ("Введите число А");
+ int userNumber1 = Convert.ToInt32 (Console.ReadLine());
+
+    Console.WriteLine ("Введите число В");
+ int userNumber2 = Convert.ToInt32 (Console.ReadLine());
+
+ int result = userNumber2 / userNumber1;
+ if  (result == userNumber1)
+ { 
+    Console.WriteLine ($"Число {userNumber1} является корнем {userNumber2}");
+ }
+
+ else
+ {
+ Console.WriteLine($"Число {userNumber1} не является корнем {userNumber2}");
+ }
