622732d87a46a3689aef172ba7eb1cc684249175
Владимир Капитаненко <vladimir_90@bk.ru>
2022-10-08 15:13:49 +0700

    new

---
+++ Les_2_Work_15/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine ("Введите день недели (цифрой)");
+int userNumberWeek = Convert.ToInt32 (Console.ReadLine());
+
+string GetNumberWeek (int userNumberWeek)
+{
+    string resultString;
+    if (userNumberWeek == 6 || userNumberWeek == 7)
+    {resultString = "Выходной день";
+    }
+    else resultString = "Будний день";
+    return resultString;
+}
+
+string result = GetNumberWeek (userNumberWeek);
+Console.WriteLine (result);
