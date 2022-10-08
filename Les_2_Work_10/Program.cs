c2f3f3726b5378fd8c3b947dd966699b0c185f1a
Владимир Капитаненко <vladimir_90@bk.ru>
2022-10-08 15:13:29 +0700

    new

---
+++ Les_2_Work_10/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 
+*/
+    Console.WriteLine ("Введите 3х значное число:"); 
+    int userNumber = Convert.ToInt32 (Console.ReadLine());
+
+int GetTwoDigitFromNumber (int userNumber)
+{
+    int number = userNumber / 10;
+    return (number % 10);
+}
+
+int result = GetTwoDigitFromNumber (userNumber);
+Console.WriteLine (result);
