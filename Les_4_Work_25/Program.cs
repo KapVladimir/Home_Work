5cd2dad120d281d450674fc86fd33be6b8132f35
Владимир Капитаненко <vladimir_90@bk.ru>
2022-10-09 23:01:17 +0700

    les_4_work_25

---
+++ Les_4_Work_25/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+int getDigitExponent (int digite, int exponent)
+{
+    int result = 1;
+    for (int i = 1; i <= exponent; i++)
+    {
+        result = result * digite;
+    }
+    return result;
+}
+
+Console.Write ("Введите число:");
+int userDigite = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите степень:");
+int userExponent = Convert.ToInt32(Console.ReadLine());
+int resultExponent = getDigitExponent (userDigite, userExponent);
+Console.Write ($"Число {userDigite} в степени {userExponent}, равно: {resultExponent}");
