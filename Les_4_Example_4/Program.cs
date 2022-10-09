f7ed0227e934bc5a1e1d8a4fdfaaf555ca13216a
Владимир Капитаненко <vladimir_90@bk.ru>
2022-10-09 23:53:10 +0700

    Les_4_Example_4

---
+++ Les_4_Example_4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+int getSumNumber (int startPoint, int endPoint)
+{
+    int result = 0;
+    if (startPoint>endPoint)
+    {
+        int buff = startPoint;
+        startPoint = endPoint;
+        endPoint = buff;
+    }
+    for (int i = startPoint; i <= endPoint; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+Console.Write ("Введите число: ");
+int userEndPoint = Convert.ToInt32 (Console.ReadLine());
+int resultFunction = getSumNumber (1, userEndPoint);
+Console.Write ($"Сумма чисел от 1 до {userEndPoint} равна: {resultFunction}");
