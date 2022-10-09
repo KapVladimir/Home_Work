a6a49d56d009b1372378405e16aa8fa0aafdbaf9
Владимир Капитаненко <vladimir_90@bk.ru>
2022-10-09 15:18:26 +0700

    les_3_example_5

---
+++ Les_3_Example_5/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N. 5 -> 1, 4, 9, 16, 25. 2 -> 1,4
+*/
+
+void tableFromDigit (int userNumber)
+{
+    int [] array = new int[userNumber];
+    int count = userNumber;
+    for (int i = 1; i < userNumber+1; i++)
+    {
+        Console.WriteLine ($"{Math.Pow(i,2)}");
+    }
+    Console.WriteLine();    
+} 
+
+Console.WriteLine ("Введите N:");
+int userNumber1 = Convert.ToInt32 (Console.ReadLine());
+tableFromDigit (userNumber1);
