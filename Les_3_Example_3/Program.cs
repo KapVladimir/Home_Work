76afa0ace240552d41408254b99ef2858a94dfde
Владимир Капитаненко <vladimir_90@bk.ru>
2022-10-09 15:17:53 +0700

    les_3_example_3

---
+++ Les_3_Example_3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y). */
+
+
+string GetQuaterFromCoordinate (int chetvert)
+{
+    string result = string.Empty;
+    if (chetvert == 1) result = "координаты X>0, Y>0";
+    else if (chetvert == 2) result = "координаты X<0, Y>0";
+        else if (chetvert == 3) result = "координаты X<0, Y<0";
+            else if (chetvert == 4) result = "координаты X>0, Y<0";
+                else result = "Введено не корректное значение четверти";
+    return result;
+}
+Console.WriteLine ("Введите номер четверти");
+int chetvert = Convert.ToInt32 (Console.ReadLine());
+string otvet = GetQuaterFromCoordinate (chetvert);
+Console.WriteLine (otvet);
