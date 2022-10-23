d15b0d63c25607f252b8be0c8ce220476e9828b6
Владимир Капитаненко <vladimir_90@bk.ru>
2022-10-23 14:06:22 +0700

    Les_9_Work_64_V2

---
+++ Les_9_Work_64_V2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+void printNumber (int userM, int userN )
+{
+    if (userM == userN)
+    {
+        Console.Write ($"{userM}");
+        return;
+    }
+    Console.Write ($"{userM}, ");
+    printNumber (userM+1,userN);
+
+}
+Console.Write("Введите число M: ");
+int userNumberM=Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число N: ");
+int userNumberN=Convert.ToInt32 (Console.ReadLine());
+Console.Write("Массив натуральных чисел: ");
+printNumber (userNumberM,userNumberN);
